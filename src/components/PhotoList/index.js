36966dda4a79fb7b702b640f10b73a59579de901
Kenzi Nielsen <kenzinielsen@gmail.com>
2021-12-03 19:27:27 -0700

    modal tests

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -106,11 +106,12 @@ const PhotoList = ({ category }) => {
   const currentPhotos = photos.filter((photo) => photo.category === category);
   const toggleModal = (image, i) => {
     setCurrentPhoto({...image, index:i});
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
   return (
     <div>
-      {isModalOpen && <Modal onClose={toggleModal} currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+      <Modal onClose={toggleModal} currentPhoto={currentPhoto} />)}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
